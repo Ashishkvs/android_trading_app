56883adc97b0579f46cc8e0504c9281fbb68f0a3
ashish <ashishkvs@github.com>
2018-03-16 13:11:10 +0530

    reg image upload added

---
+++ app/src/main/java/com/datazi/CheckUser.java
@@ -0,0 +1,8 @@
+package com.datazi;
+
+/**
+ * Created by Ashish on 3/13/2018.
+ */
+
+public class CheckUser {
+}
---
+++ app/src/main/java/com/datazi/UserService.java
@@ -0,0 +1,8 @@
+package com.datazi;
+
+/**
+ * Created by Ashish on 3/15/2018.
+ */
+
+public class UserService {
+}
